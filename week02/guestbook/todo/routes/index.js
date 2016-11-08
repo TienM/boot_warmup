a21f3047847d58b550dcfd87d17026e13c8c07d0
TienM <tienleigh@yahoo.com>
2016-11-08 17:25:24 -0600

    Fixed viewing issue

--- week02/guestbook/todo/routes/index.js
+++ week02/guestbook/todo/routes/index.js
@@ -6,3 +6,12 @@ var Todo = mongoose.model('Todo');
  exports.index = function ( req, res ){
    res.render( 'index', { title : 'Express Todo Example' });
  };
+
+ exports.create = function ( req, res ){
+   new Todo({
+     content    : req.body.content,
+     updated_at : Date.now()
+   }).save( function( err, todo, count ){
+     res.redirect( '/' );
+   });
+ };
