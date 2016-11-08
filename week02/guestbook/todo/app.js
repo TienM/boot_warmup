78c5d186d3bca1779eaaeeb4824de328240d4a4c
TienM <tienleigh@yahoo.com>
2016-11-08 17:12:10 -0600

    Got input working

--- week02/guestbook/todo/app.js
+++ week02/guestbook/todo/app.js
@@ -14,7 +14,6 @@ app.set( 'views', path.join( __dirname, 'views' ));
 app.set( 'view engine', 'ejs' );
 app.use( express.favicon());
 app.use( express.logger( 'dev' ));
-app.use( express.bodyParser());
 app.use( express.json());
 app.use( express.urlencoded());
 app.use( express.methodOverride());
@@ -27,7 +26,6 @@ if ( 'development' == app.get( 'env' )) {
 }
 
 app.get( '/', routes.index );
-app.post( '/create', routes.create );
 
 http.createServer( app ).listen( app.get( 'port' ), function(){
   console.log( 'Express server listening on port ' + app.get( 'port' ));
--- week02/guestbook/todo/routes/index.js
+++ week02/guestbook/todo/routes/index.js
@@ -6,12 +6,3 @@ var Todo = mongoose.model('Todo');
  exports.index = function ( req, res ){
    res.render( 'index', { title : 'Express Todo Example' });
  };
-
- exports.create = function ( req, res ){
-   new Todo({
-     content    : req.body.content,
-     updated_at : Date.now()
-   }).save( function( err, todo, count ){
-     res.redirect( '/' );
-   });
- };
